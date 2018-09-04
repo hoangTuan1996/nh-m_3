bf3491003b6c45d572464d2eed29cfe9b34e11a7
t.anh <4evernavi242@gmail.com>
2018-09-04 19:10:23 +0700

    w2

---
+++ js/coach.js
@@ -0,0 +1,142 @@
+var coach = [
+	{
+		"avatar": "Avatar",
+		"link": "Link",
+		"name": "Name",
+		"age": "Age",
+		"year": "EXP Year",
+		"country": "Country",
+		"sport": "Sport",
+		"comment": "Comment"
+	},
+	{
+		"avatar": "Avatar",
+		"link": "Link",
+		"name": "Name",
+		"age": "Age",
+		"year": "EXP Year",
+		"country": "Country",
+		"sport": "Sport",
+		"comment": "Comment"
+	},
+	{
+		"avatar": "Avatar",
+		"link": "Link",
+		"name": "Name",
+		"age": "Age",
+		"year": "EXP Year",
+		"country": "Country",
+		"sport": "Sport",
+		"comment": "Comment"
+	},
+	{
+		"avatar": "Avatar",
+		"link": "Link",
+		"name": "Name",
+		"age": "Age",
+		"year": "EXP Year",
+		"country": "Country",
+		"sport": "Sport",
+		"comment": "Comment"
+	},
+	{
+		"avatar": "Avatar",
+		"link": "Link",
+		"name": "Name",
+		"age": "Age",
+		"year": "EXP Year",
+		"country": "Country",
+		"sport": "Sport",
+		"comment": "Comment"
+	},
+	{
+		"avatar": "Avatar",
+		"link": "Link",
+		"name": "Name",
+		"age": "Age",
+		"year": "EXP Year",
+		"country": "Country",
+		"sport": "Sport",
+		"comment": "Comment"
+	},
+	{
+		"avatar": "Avatar",
+		"link": "Link",
+		"name": "Name",
+		"age": "Age",
+		"year": "EXP Year",
+		"country": "Country",
+		"sport": "Sport",
+		"comment": "Comment"
+	},
+	{
+		"avatar": "Avatar",
+		"link": "Link",
+		"name": "Name",
+		"age": "Age",
+		"year": "EXP Year",
+		"country": "Country",
+		"sport": "Sport",
+		"comment": "Comment"
+	},
+	{
+		"avatar": "Avatar",
+		"link": "Link",
+		"name": "Name",
+		"age": "Age",
+		"year": "EXP Year",
+		"country": "Country",
+		"sport": "Sport",
+		"comment": "Comment"
+	},
+	{
+		"avatar": "Avatar",
+		"link": "Link",
+		"name": "Name",
+		"age": "Age",
+		"year": "EXP Year",
+		"country": "Country",
+		"sport": "Sport",
+		"comment": "Comment"
+	},
+	{
+		"avatar": "Avatar",
+		"link": "Link",
+		"name": "Name",
+		"age": "Age",
+		"year": "EXP Year",
+		"country": "Country",
+		"sport": "Sport",
+		"comment": "Comment"
+	},
+	{
+		"avatar": "Avatar",
+		"link": "Link",
+		"name": "Name",
+		"age": "Age",
+		"year": "EXP Year",
+		"country": "Country",
+		"sport": "Sport",
+		"comment": "Comment"
+	},
+	{
+		"avatar": "Avatar",
+		"link": "Link",
+		"name": "Name",
+		"age": "Age",
+		"year": "EXP Year",
+		"country": "Country",
+		"sport": "Sport",
+		"comment": "Comment"
+	},
+	{
+		"avatar": "Avatar",
+		"link": "Link",
+		"name": "Name",
+		"age": "Age",
+		"year": "EXP Year",
+		"country": "Country",
+		"sport": "Sport",
+		"comment": "Comment"
+	}
+]
---
+++ js/namecoach.js
@@ -0,0 +1,142 @@
+var namecoach = [
+	{
+		"avatar": "img/coach/Roach.png",
+		"link": "#",
+		"name": "Freddie Roach",
+		"age": 56,
+		"year": 21,
+		"country": "American",
+		"sport": "Boxing",
+		"comment": "Frederick Steven Freddie Roach (born March 5, 1960) is an American boxing trainer and former professional boxer. Roach is widely regarded as one of the best boxing trainers of all time."
+	},
+	{
+		"avatar": "img/coach/Abel.png",
+		"link": "#",
+		"name": "Abel Sanchez", 
+		"age": 54,
+		"year": 14,
+		"country": "Mexico",
+		"sport": "Boxing",
+		"comment": "Abel Sanchez is a Mexican boxing trainer. He is best known as the coach of Gennady Golovkin, and has trained other boxers such as Lupe Aquino, Terry Norris, and Murat Gassiev.[1] Sanchez won the Futch-Condon Award in 2015."
+	},
+	{
+		"avatar": "img/coach/Luis.png",
+		"link": "#",
+		"name": "Luis Siles", 
+		"age": 47,
+		"year": 11,
+		"country": "Spain",
+		"sport": "Chess",
+		"comment": "Maestro FIDE y entrenador de ajedrez. Editor y director de Capakhine, la revista de ajedrez para los niños y sus padres. Siempre pensando en crear contenidos que puedan ser útiles para el progreso ajedrecístico. Creativo, bloguero, youtuber y un poco friki."
+	},
+	{
+		"avatar": "img/coach/Dmov.png",
+		"link": "#",
+		"name": "Dmov", 
+		"age": 36,
+		"year": 13,
+		"country": "Italy",
+		"sport": "Chess",
+		"comment": "She won the Italian Women Chess Championship twice, in 2015 and 2016, and She played with the Italian national team at the European Team Championship of 2015 and at the Olympiads of 2016. Currently she has two WIM norms."
+	},
+	{
+		"avatar": "img/coach/Manh.png",
+		"link": "#",
+		"name": "Nguyen Huy Manh", 
+		"age": 38,
+		"year": 8,
+		"country": "Viet Nam",
+		"sport": "Swim",
+		"comment": "Huy Manh graduated from Hanoi University of Pedagogy, Faculty of Physical Education. Since he was in the lecture hall, Manh has participated in swimming lessons at many centers to earn more income, expenses for living, school. So far, this swim instructor has nearly 10 years working experience."
+	},
+	{
+		"avatar": "img/coach/Pruitt.png",
+		"link": "#",
+		"name": "Jeremy Pruitt", 
+		"age": 47,
+		"year": 12,
+		"country": "Ala",
+		"sport": "Football",
+		"comment": "Pruitt's first collegiate defensive coordinator job came in 2013, when he was hired at Florida State. The Seminoles won the national championship and ranked third in the country in total defense under his leadership."
+	},
+	{
+		"avatar": "img/coach/Peter.png",
+		"link": "#",
+		"name": "Peter Pan", 
+		"age": 36,
+		"year": 6,
+		"country": "England",
+		"sport": "Tennis",
+		"comment": "Peter graduated from England University of Pedagogy, Faculty of Physical Education. Since he was in the lecture hall, Peter has participated in swimming lessons at many centers to earn more income, expenses for living, school. So far, this swim instructor has nearly 10 years working experience."
+	},
+	{
+		"avatar": "img/coach/Mulyo.png",
+		"link": "#",
+		"name": "Muldo Handoyo", 
+		"age": 63,
+		"year": 23,
+		"country": "Indonesia",
+		"sport": "Badminton",
+		"comment": "Indonesian coach Mulyo Handoyo, whose services were roped in by the Sports Authority of India early this year, is unlikely to return to India and continue to assist chief national coach P. Gopi Chand, according to a senior official of the Badminton Association of India."
+	},
+	{
+		"avatar": "img/coach/Rick.png",
+		"link": "#",
+		"name": "Rick Henderson", 
+		"age": 59,
+		"year": 17,
+		"country": "Brasil",
+		"sport": "Volleyball",
+		"comment": "Henderson might not be a household name like Michael Phelps, Usain Bolt or Rafael Nadal but in volleyball-mad Brazil the veteran coach cannot move around his native city without being stopped for autographs and selfies."
+	},
+	{
+		"avatar": "img/coach/larry.png",
+		"link": "#",
+		"name": "Larry Brown", 
+		"age": 65,
+		"year": 25,
+		"country": "Indonesia",
+		"sport": "Basketball",
+		"comment": "The Jewish Coaches Association—the group of Jewish college basketball coaches that convenes annually at the Final Four—has lost one of its most prominent members … and gained an even more prominent one. "
+	},
+	{
+		"avatar": "img/coach/chris.png",
+		"link": "#",
+		"name": "Chris Rupp", 
+		"age": 52,
+		"year": 14,
+		"country": "Brasil",
+		"sport": "Baseball",
+		"comment": "Chris Rupp was recently named as the new Deer Park High School head baseball coach. He has 31 years of coaching experience at the high school, junior college and collegiate levels."
+	},
+	{
+		"avatar": "img/coach/pan.png",
+		"link": "#",
+		"name": "Chris Pan", 
+		"age": 43,
+		"year": 10,
+		"country": "England",
+		"sport": "Cycle",
+		"comment": "If you’re training for the upcoming Triathlon/Cycling season and your time is limited, Training Plans by ChrisPanTraining can maximise your performance!"
+	},
+	{
+		"avatar": "img/coach/linton.png",
+		"link": "#",
+		"name": "Linton Fafie",
+		"age": 41,
+		"year": 9,
+		"country": "Australia",
+		"sport": "Surf",
+		"comment": "More than your standard learn-to-surf school, Byron Bay’s Next Level Surf Coaching employs a 2:1 surfer-to-certified-coach ratio. 50-year-old grom James decided to test it out."
+	},
+	{
+		"avatar": "img/coach/Malphite.png",
+		"link": "#",
+		"name": "Malphite",
+		"age": 41,
+		"year": 7,
+		"country": "Egyptian",
+		"sport": "Gym",
+		"comment": "The standard deduction works out to be more than itemized deductions for many taxpayers, so itemizing isn’t always a good deal."
+	}
+]
